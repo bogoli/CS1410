40f27a7385f56534fb721026e55e76fa63d65aa1
Lia Bogoev <lia.bogoev@gmail.com>
2014-01-18 22:41:22 -0700

    Re-adding Friend.cpp because i'm not sure if git is behaving

--- 1/Friend.cpp
+++ 1/Friend.cpp
@@ -101,7 +101,7 @@ void SearchInterest (Friend friends[], int size, string keywords){
 	// keywords = tolower(keywords);
 	for (int i = 0; i < size; i++){
 	// 	friends[i].interests = tolower(friends[i].interests);
-		if (friends[i].interests.compare(keywords) >= 0){
+		if (friends[i].interests.compare(keywords) == 0){
 			cout << "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - \n";
 			cout << "    # " << i << endl;
 			cout << "    name:      " << friends[i].screenName << endl;
