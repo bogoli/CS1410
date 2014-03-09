742ee19f146b710812e60d07d3af5185715a6f47
Lia Bogoev <lia.bogoev@gmail.com>
2014-03-09 17:16:08 -0600

    Adding assignment 6 description and file stream shenanigans.

--- 6/main.cpp
+++ 6/main.cpp
@@ -1,16 +1,63 @@
+// Lia Bogoev 
+// CS 1410 - Assignment 6
+
 #include <iostream>
+#include <fstream>
+
+#include <cstring>
+#include <string> 
+
 using namespace std;
 
 int main(){
-	extern char **environ;
+	// output the progrmaming environment to a file
+	ofstream fout("environment.txt");
 
+	// this is the stuff that reads in the programming environment
+	extern char **environ;
 	int k = 0;
 
 	while (environ[k] != NULL) {
-		cout << environ[k] << endl;
+		// output to the file
+		fout << environ[k] << endl;
+		// output to the console
+		cout << "\t" << environ[k] << endl;
 		k++; 
 	}
 
+
+	// use the same output file as the input file 
+	ifstream fin;
+	fin.open("environment.txt"); 
+
+	if(fin.fail()){
+		cout << "Could not find file.\n";
+	}
+	else{
+		cout << "The file exists\n";
+
+		char line[100];
+		fin.getline(line,100);
+
+		
+
+
+
+
+
+
+
+
+
+		
+
+		// cout << line << endl;
+	}
+
+
+	// close the file
+	fout.close(); 
+	fin.close();
 
 return 0;
 
