fefd87d3e0e3a8a07e1d58810a927eb591f92010
Lia Bogoev <lia.bogoev@gmail.com>
2014-03-11 14:03:55 -0600

    Working word count --> eliminates strings that don't have letters and strings that are <2 in length

--- 6/main.cpp
+++ 6/main.cpp
@@ -6,11 +6,13 @@
 
 #include <cstring>
 #include <string>
-
+#include <cstddef>
 #include <iomanip>
 #include <map>
 
 using namespace std;
+
+const string ALPHABET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 int main(){
 	// output the progrmaming environment to a file
@@ -58,56 +60,82 @@ int main(){
 			string lineStr(line);
 			for(int i = 0; i < lineStr.length(); ++i){
 				// cout << lineStr.at(i) << " "; 
-				if(isalpha(lineStr.at(i)) ){
-
+				if(isalnum(lineStr.at(i)) && !isspace(lineStr.at(i))){
 					// cout << lineStr.at(i) << ".....y" << endl; 
 					temp.append(1, lineStr.at(i));
 
 				}
-				if(!isalpha(lineStr.at(i))){
+				if(!isalnum(lineStr.at(i)) ){
 					// cout << "\t\tn" << endl;
 					if(temp.length() > 1){
-						word = temp;
-						temp = "";
-						words[word]+=1;
-					}
+						
+						/*
+						for(int j = 0; j < temp.length(); ++j){
+							if(isalpha(temp.at(i))){
+								// if there's at least one letter
+								word = temp;
+								words[word]+=1;
+								temp = "";
+							}
+							else{ 
+								// temp = "";
+							} 
+						}
+						*/
 
+						word = temp;
+						words[word]+=1;
+						temp = "";
+
+					}
 					// cout << word << " " << words[word] << endl; 
 				}
 
 			}
+			// need this for the last word
 			word = temp;
 			words[word] += 1;
 
-			// cout << word << " " << words[word] << endl; 
 
 		} // end while
 
+		for(map<string, int>::const_iterator it = words.begin(); it != words.end(); ++it){			
+			if (it->first.length() == 1){
+				// if the word is shorter than 2 erase it
+				// cout << it->first << endl;
+				words.erase(it);
+			}
+			else{
+				if( (it->first.find_first_of(ALPHABET,0)) == std::string::npos){
+					words.erase(it);
+				}
+			}
+		}// end for loop that clears out non-words
 
-		// cout << "\tTEMP IS:    " << temp << endl;
+	
 
-		/*
-		cout << "length of line: " << linestr.length() << endl;
-		cout << linestr << endl;
-		*/
+
+		int count = 0;
 		
-		// cout << line << endl;
-		
-
-		for(map<string, int>::const_iterator i = words.begin(); i != words.end(); ++i){
+		for(map<string, int>::const_iterator i = words.begin(); i != words.end(); ++i){			
+			// prints out the entire map
 			cout << fixed;
 			cout << setprecision(0);
 			cout << left << setw(20) << i->first << " " << i->second << endl;
+			++count;
 		}
+		
 
-
+		cout << "Number of words: " << count;
 	} // end else (file is open)
-
 
 	// close the file
 	fout.close(); 
 	fin.close();
 	cout << endl;
+
+
+
 return 0;
 
 }
