2915ac9602e23d52f8978b38ad1044e630217f99
Lia Bogoev <lia.bogoev@gmail.com>
2014-04-01 22:00:15 -0600

    Add char** array for maze, add maze.txt file (example)

--- 8/main.cpp
+++ 8/main.cpp
@@ -3,6 +3,7 @@
 // Assignment 8
 
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 // TASK 1
@@ -32,7 +33,6 @@ void PrintBinary(int num){
 	}
 }
 
-
 // TASK 3
 int ComputeSumOfDigits(int num){
 	if(num < 10){
@@ -44,7 +44,6 @@ int ComputeSumOfDigits(int num){
 }
 
 // TASK 4
-
 
 int main(int argc, char const *argv[])
 {
@@ -65,6 +64,54 @@ int main(int argc, char const *argv[])
 	cin >> integer;
 	cout << "Sum of digits: " << ComputeSumOfDigits(integer);
 */
+
+	fstream file("maze.txt", ios::in);
+	char ** maze;
+	int rows, cols, startRow, startCol;
+
+	if(file.is_open()){
+		cout << "File opened successfully.\n";
+
+		// get size of maze and starting coordinates
+		file >> rows;	// rows
+		file >> cols;	// columns
+		file >> startRow;	// start row
+		file >> startCol;  	// start column
+
+		// allocate the memory for the maze** 
+		maze = new char*[rows];
+		for (int i = 0; i < rows; ++ i){
+			maze[i] = new char[cols];
+		}
+
+		// ready to read in the maze
+		for(int i = 0; i < rows; ++i){
+			for(int j = 0; j < cols; ++j){
+				file >> maze[i][j];
+			}
+		}
+
+		// finished with the file
+		file.close();
+	}
+	else{
+		cout << "Error. File open failed. \n";
+	}
+
+	for(int i = 0; i < rows; ++i){
+		for(int j = 0; j < cols; ++j){
+			cout << maze[i][j];
+		}
+		cout << endl;
+	}
+
+
+
+	// destroy the maze
+	for (int i = 0; i < rows; ++ i){
+		delete maze[i];
+	}
+	delete maze;
 
 	cout << endl;
 	return 0;
