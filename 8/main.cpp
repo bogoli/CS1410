8c0b3b8edc0424d177fc68184656bc5ca1ca84f4
Lia Bogoev <lia.bogoev@gmail.com>
2014-04-03 12:13:55 -0600

    Finished assignment, implements recursive maze solving function. Adds another maze, plus original example maze.

--- 8/main.cpp
+++ 8/main.cpp
@@ -44,10 +44,53 @@ int ComputeSumOfDigits(int num){
 }
 
 // TASK 4
+bool findPath(char** m, int r, int c, int sR, int sC){
+	if (sR >= r || sC >= c){
+		// check bounds first
+		return false;
+	}
+	if(m[sR][sC] == 't'){
+		// treasure! 
+		return true;
+	}
+	
+	if(m[sR][sC] == '.'){
+
+		m[sR][sC] = '+'; 
+
+		if(findPath(m, r, c, sR+1, sC) == true){
+			// north
+			cout << sR << " " << sC << endl;
+	 		return true;
+		}
+		
+		if(findPath(m, r, c, sR, sC+1) == true){
+			// west
+			cout << sR << " " << sC << endl;
+	 		return true;
+		}
+
+		if(findPath(m, r, c, sR-1, sC) == true){
+			// south
+			cout << sR << " " << sC << endl;
+	 		return true;
+		}
+
+
+		if(findPath(m, r, c, sR, sC-1) == true){
+			// east
+			cout << sR << " " << sC << endl;
+	 		return true;
+		}
+	}
+
+	return false;
+}
+
 
 int main(int argc, char const *argv[])
 {
-/*
+
 	cout << "How many levels did the cannonball pyramid have? ";
 	int levels;
 	cin >> levels;
@@ -58,19 +101,20 @@ int main(int argc, char const *argv[])
 	cin >> number;
 	cout << "Binary: ";
 	PrintBinary(number);
+	cout << endl << endl;
 
 	cout << "Input a (large-ish) integer: ";
 	int integer;
 	cin >> integer;
 	cout << "Sum of digits: " << ComputeSumOfDigits(integer);
-*/
+
 
 	fstream file("maze.txt", ios::in);
 	char ** maze;
 	int rows, cols, startRow, startCol;
 
 	if(file.is_open()){
-		cout << "File opened successfully.\n";
+		cout << "\n\nFile opened successfully.\n";
 
 		// get size of maze and starting coordinates
 		file >> rows;	// rows
@@ -98,13 +142,59 @@ int main(int argc, char const *argv[])
 		cout << "Error. File open failed. \n";
 	}
 
+
+	// output the maze
 	for(int i = 0; i < rows; ++i){
 		for(int j = 0; j < cols; ++j){
 			cout << maze[i][j];
 		}
 		cout << endl;
 	}
+	cout << endl;
 
+
+	// solve the maze
+	int solved = findPath(maze, rows, cols, startRow, startCol);
+
+/*
+Mazes tested: 
+// maze that I created
+	15 15
+	1 2 
+	XXXXXXXXXXXXXXX
+	XX.XXXXXX....XX
+	XX....XXX.XX.XX
+	XXXX.XXXX....XX
+	XX...XXXX.XX.XX
+	XXXX.X....XX.XX
+	XXX..X.XXXXX.XX
+	XXX.XX.XX....XX
+	XXX....XX.XXXXX
+	XXXXXXXXX...XXX
+	XXX.......XXXXX
+	XXX.XX.XXX...XX
+	XXX....XXX.X.XX
+	XXXXXX.....XtXX
+	XXXXXXXXXXXXXXX
+
+// example maze
+	11 10
+	9 4 
+	XXXXXXXXXX
+	XX...tX..X 
+	XX.XXXX.XX 
+	XX.......X 
+	XXXXXXX.XX 
+	X....XX.XX 
+	X.XX.....X 
+	X...XX.XXX 
+	XXX..XXXXX 
+	XXXX....XX 
+	XXXXXXXXXX
+
+
+
+*/
 
 
 	// destroy the maze
